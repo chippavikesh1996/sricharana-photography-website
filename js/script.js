d8635b80047076161cdc34cb72bdfb23123cccff
vikesh@Github1996 <chippavikesh1996@gmail.com>
2023-02-19 11:24:01 +0530

    sri charana fashion studio code

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+  ///// Nav bar BG CSS
+  // $(document).ready(function () {
+  //   var scroll_start = 0;
+  //   var startchange = $('#startchange');
+  //   var offset = startchange.offset();
+  //   if (startchange.length) {
+  //     $(document).scroll(function () {
+  //       scroll_start = $(this).scrollTop();
+  //       if (scroll_start > offset.top) {
+  //         $("#navbar-color").css('background-color', '#000000e0');
+  //       } else {
+  //         $('#navbar-color').css('background-color', 'transparent');
+  //       }
+  //     });
+  //   }
+  // });
